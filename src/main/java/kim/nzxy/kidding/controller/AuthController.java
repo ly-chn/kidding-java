3c3cc5d3744a126cb451244a5d30c2a2b6dd519f
ly-chn <3293232930@qq.com>
2023-08-10 18:11:41 +0800

    add doc. add register method

--- src/main/java/kim/nzxy/kidding/controller/AuthController.java
+++ src/main/java/kim/nzxy/kidding/controller/AuthController.java
@@ -2,6 +2,7 @@ package kim.nzxy.kidding.controller;
 
 import cn.dev33.satoken.stp.StpUtil;
 import kim.nzxy.kidding.dto.UserLoginDTO;
+import kim.nzxy.kidding.dto.UserRegisterDTO;
 import kim.nzxy.kidding.util.AdminWorkplace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +25,7 @@ public class AuthController {
 
     @PostMapping("login")
     public String login(@RequestBody UserLoginDTO loginInfo) {
-        boolean enable = adminWorkplace.readBool(String.format("账号: %s, 密码: %s, 是否允许登录? ",
+        boolean enable = adminWorkplace.readBool(String.format("账号: %s, 密码: %s 请求登录, 是否允许? ",
                         loginInfo.getUsername(),
                         loginInfo.getPassword()),
                 Duration.ofSeconds(10));
@@ -38,5 +39,23 @@ public class AuthController {
     @GetMapping("login")
     public String loginForGet(UserLoginDTO loginInfo) {
         return login(loginInfo);
+    }
+
+    @PostMapping("register")
+    public String register(@RequestBody UserRegisterDTO registerInfo) {
+        boolean enable = adminWorkplace.readBool(String.format("账号: %s, 密码: %s 申请注册, 是否允许? ",
+                        registerInfo.getUsername(),
+                        registerInfo.getPassword()),
+                Duration.ofSeconds(10));
+        if (enable) {
+            StpUtil.login(registerInfo.getUsername());
+            return "注册成功, 您可以使用该账号登录了";
+        }
+        return "管理员拒绝了您的注册请求";
+    }
+
+    @GetMapping("register")
+    public String registerForGet(UserRegisterDTO registerInfo) {
+        return register(registerInfo);
     }
 }
